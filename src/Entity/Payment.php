8db7cc67534c6aa0a8460de929d19c96582dd81c
malek304 <malek.bouderbela@esprit.tn>
2026-02-10 09:53:43 +0100

    shop

---
+++ src/Entity/Payment.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaymentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaymentRepository::class)]
+class Payment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $paymentDate = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $amount = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $paymentMethod = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $paymentStatus = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $transactionRef = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPaymentDate(): ?\DateTimeImmutable
+    {
+        return $this->paymentDate;
+    }
+
+    public function setPaymentDate(\DateTimeImmutable $paymentDate): static
+    {
+        $this->paymentDate = $paymentDate;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getPaymentMethod(): ?string
+    {
+        return $this->paymentMethod;
+    }
+
+    public function setPaymentMethod(string $paymentMethod): static
+    {
+        $this->paymentMethod = $paymentMethod;
+
+        return $this;
+    }
+
+    public function getPaymentStatus(): ?string
+    {
+        return $this->paymentStatus;
+    }
+
+    public function setPaymentStatus(string $paymentStatus): static
+    {
+        $this->paymentStatus = $paymentStatus;
+
+        return $this;
+    }
+
+    public function getTransactionRef(): ?string
+    {
+        return $this->transactionRef;
+    }
+
+    public function setTransactionRef(string $transactionRef): static
+    {
+        $this->transactionRef = $transactionRef;
+
+        return $this;
+    }
+}
