8f2a28b9dffad3c0b2b0c8800fd96c42cae263b3
malek304 <malek.bouderbela@esprit.tn>
2026-02-10 09:53:43 +0100

    shop

---
+++ migrations/Version20260207235920.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260207235920 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE payment (id INT AUTO_INCREMENT NOT NULL, payment_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', amount NUMERIC(10, 2) NOT NULL, payment_method VARCHAR(20) NOT NULL, payment_status VARCHAR(20) NOT NULL, transaction_ref VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE shop_item (id INT AUTO_INCREMENT NOT NULL, item_name VARCHAR(255) NOT NULL, item_description LONGTEXT NOT NULL, item_category VARCHAR(50) NOT NULL, price NUMERIC(10, 2) NOT NULL, quantity INT NOT NULL, total_price NUMERIC(10, 2) NOT NULL, purchase_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', status VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE payment');
+        $this->addSql('DROP TABLE shop_item');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ src/Entity/ShopItem.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShopItemRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShopItemRepository::class)]
+class ShopItem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $itemName = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $itemDescription = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $itemCategory = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $totalPrice = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $purchaseDate = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemName(): ?string
+    {
+        return $this->itemName;
+    }
+
+    public function setItemName(string $itemName): static
+    {
+        $this->itemName = $itemName;
+
+        return $this;
+    }
+
+    public function getItemDescription(): ?string
+    {
+        return $this->itemDescription;
+    }
+
+    public function setItemDescription(string $itemDescription): static
+    {
+        $this->itemDescription = $itemDescription;
+
+        return $this;
+    }
+
+    public function getItemCategory(): ?string
+    {
+        return $this->itemCategory;
+    }
+
+    public function setItemCategory(string $itemCategory): static
+    {
+        $this->itemCategory = $itemCategory;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getTotalPrice(): ?string
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice(string $totalPrice): static
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    public function getPurchaseDate(): ?\DateTimeImmutable
+    {
+        return $this->purchaseDate;
+    }
+
+    public function setPurchaseDate(\DateTimeImmutable $purchaseDate): static
+    {
+        $this->purchaseDate = $purchaseDate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+    
+}
